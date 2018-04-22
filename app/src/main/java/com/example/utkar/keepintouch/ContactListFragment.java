57a3dd75d462293453b8264dd04ddc7876fe7681
utk.bajpai <utk.bajpai@samsung.com>
2018-04-23 01:22:09 +0530

    created list layout for contatcs, added butterknife and list view

--- app/src/main/java/com/example/utkar/keepintouch/ContactListFragment.java
+++ app/src/main/java/com/example/utkar/keepintouch/ContactListFragment.java
@@ -1,12 +1,67 @@
 package com.example.utkar.keepintouch;
 
-import android.app.Fragment;
-import android.support.v7.widget.RecyclerView;
+import android.annotation.SuppressLint;
+import android.database.Cursor;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
-public class ContactListFragment  extends Fragment{
-private RecyclerView listView;
-private ArrayList contactList;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ContactListFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor>,
+        AdapterView.OnItemClickListener {
+    @SuppressLint("InlinedApi")
+    private final static String[] FROM_COLUMNS = {
+            Build.VERSION.SDK_INT
+                    >= Build.VERSION_CODES.HONEYCOMB ?
+                    ContactsContract.Contacts.DISPLAY_NAME_PRIMARY :
+                    ContactsContract.Contacts.DISPLAY_NAME
+    };
+    @BindView(R.id.contact_list)ListView listView;
+    private ArrayList contactList;
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.layout_contact_list, container , false );
+        ButterKnife.bind(this,view);
+        return view;
+    }
+
 
 }
