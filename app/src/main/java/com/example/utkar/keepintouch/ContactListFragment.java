543f48bafcfeb4b43b92f205f580006329f036e1
utk.bajpai <utk.bajpai@samsung.com>
2018-04-22 23:50:59 +0530

    adding repository

---
+++ app/src/main/java/com/example/utkar/keepintouch/ContactListFragment.java
@@ -0,0 +1,12 @@
+package com.example.utkar.keepintouch;
+
+import android.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ContactListFragment  extends Fragment{
+private RecyclerView listView;
+private ArrayList contactList;
+
+}
